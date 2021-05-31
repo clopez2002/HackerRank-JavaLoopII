a76f1db1982fd09e2eb714f47d018e89105132c1
lopezlin <morgan_pirata2003@yahoo.com.ar>
2021-05-31 18:46:55 -0300

    se completa la tarea

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,30 @@
-public class Solution {
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int j=0;
+            int suma = a;
+
+            for (j=0; j<n; j++)
+                {
+                   suma +=  Math.pow(2,j)*b;
+                   System.out.print(suma+" ");
+                }
+
+
+
+            System.out.println("\na = " + a);
+            System.out.println("b = " +b);
+            System.out.println("n = "+n);
+
+        }
+        in.close();
+    }
 }
